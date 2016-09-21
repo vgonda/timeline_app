d59d948c0b815a89710c5d6b1c70e8d9dade815f
Victoria Gonda <v.m.gonda@gmail.com>
2016-09-21 19:00:15 -0400

    Allow deleting an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,11 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
+  def destroy
+    Event.find(params[:id]).destroy
+    redirect_to events_path
+  end
+
   private
 
   def event_params
