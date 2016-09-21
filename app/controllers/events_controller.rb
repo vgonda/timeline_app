8367a29988b9360e2d50fde85186dfef54db6acc
Victoria Gonda <v.m.gonda@gmail.com>
2016-09-21 18:38:42 -0400

    Add Event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,19 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    Event.create(event_params)
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:message)
+  end
 end
